ff6fad964883d9490fa203167ba8cceb62878c99
C2_4b <4barbes@gmail.com>
2014-05-06 11:53:51 +0200

    Initial commit : build architecture

---
+++ wos.py
@@ -0,0 +1,17 @@
+import requests
+import spynner
+
+def set_session():
+	return
+
+def get_credentials():
+	return
+
+def post_auth():
+	return
+
+def init_query():
+	return 
+
+def extract():
+	return
